fbc2257947c92eaf7d177a48f4ec11cd440efc14
thxssio <thassiogomes44@gmail.com>
2023-08-14 16:46:45 -0300

    ex017 em java :(

---
+++ ex017.java
@@ -0,0 +1,13 @@
+public class funcionario{
+    String Nome;
+    String Cargo;
+    int Salario;
+
+    void inicializar(String N, String C, int S){
+        Nome =  N;
+        Cargo = C;
+        Salario = S;
+    }
+
+
+}   
