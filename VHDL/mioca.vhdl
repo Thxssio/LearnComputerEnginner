5ec7178b281aff43fe214d1c34f7f536e742f7b2
Thássio Silva <95764952+Thxssio@users.noreply.github.com>
2023-10-08 16:56:56 -0300

    Update mioca.vhdl

--- VHDL/mioca.vhdl
+++ VHDL/mioca.vhdl
@@ -4,7 +4,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity mioca is
     Port (
         clk  : in  STD_LOGIC;
-        sel  : out STD_LOGIC_VECTOR (1 downto 0);
+        sel  : out STD_LOGIC_VECTOR (3 downto 0);
         disp : out STD_LOGIC_VECTOR (3 downto 0)
     );
 end mioca;
@@ -26,18 +26,19 @@ begin
 
         if cont >= tempo * 4 then
             disp <= "1000"; -- a
-            sel <= "01";     -- disp 2
+            sel <= "1110";     -- disp 1
         elsif cont >= tempo * 3 then
             disp <= "0100"; -- b
-            sel <= "01";     -- disp 2
+            sel <= "1110";     -- disp 1
         elsif cont >= tempo * 2 then
             disp <= "0010"; -- c
-            sel <= "10";     -- disp 3
+            sel <= "1110";     -- disp 1
         elsif cont >= tempo then
             disp <= "0001"; -- d
-            sel <= "10";     -- disp 3
+            sel <= "1110";     -- disp 1
         end if;
     end if;
 end process;
 
 end Behavioral;
+
