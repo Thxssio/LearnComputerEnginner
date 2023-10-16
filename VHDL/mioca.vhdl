b553abe4cbb2e90bd0d220bcea0e540b18a09ddc
Thássio Silva <95764952+Thxssio@users.noreply.github.com>
2023-10-16 14:50:04 -0300

    Update mioca.vhdl

--- VHDL/mioca.vhdl
+++ VHDL/mioca.vhdl
@@ -2,43 +2,199 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity mioca is
-    Port (
-        clk  : in  STD_LOGIC;
-        sel  : out STD_LOGIC_VECTOR (3 downto 0);
-        disp : out STD_LOGIC_VECTOR (3 downto 0)
-    );
+    Port ( clk : in  STD_LOGIC;
+           sel : out  STD_LOGIC_VECTOR (3 downto 0);
+           d : out  STD_LOGIC_VECTOR (6 downto 0));
 end mioca;
 
 architecture Behavioral of mioca is
-    constant tempo : integer := 18750000;
 
 begin
+	process(clk)
+	variable cont : integer range 0 to 300000000;
+	variable cont2: integer range 0 to 2000000;
+	begin
+	if rising_edge(clk) then
+		if cont < 300000000 then cont := cont + 1;
+			else cont := 0;
+		end if; -- cont < 300Mhz
 
-process(clk)
-    variable cont : integer range 0 to 300e6 := 0;
-begin
-    if rising_edge(clk) then
-        if cont > 0 then
-            cont := cont - 1;
-        else
-            cont := tempo * 4; -- Reinicia o contador
-        end if;
+		if cont < 300000000 * 1/16 then sel <= "0000";
+			     			d   <= "0111111";
 
-        if cont >= tempo * 4 then
-            disp <= "1000"; -- a
-            sel <= "1110";     -- disp 1
-        elsif cont >= tempo * 3 then
-            disp <= "0100"; -- b
-            sel <= "1110";     -- disp 1
-        elsif cont >= tempo * 2 then
-            disp <= "0010"; -- c
-            sel <= "1110";     -- disp 1
-        elsif cont >= tempo then
-            disp <= "0001"; -- d
-            sel <= "1110";     -- disp 1
-        end if;
-    end if;
-end process;
+		elsif cont < 300000000 * 2/16 then if cont2 < 2000000 * 1/3
+			then    sel <= "0111";
+				d   <= "0111101";
+																										  cont2 := cont2 + 1;
+		elsif cont2 < 2000000 * 2/3
+				then sel <= "1011";
+				d   <= "0111111";
+			
+		cont2 := cont2 + 1;
+			elsif cont2 < 2000000 * 3/3
+				then sel <= "1101";
+				d   <= "0111111";
+		cont2 := cont2 + 1;
+			else
+				cont2 := 0;
+		end if;
+
+		elsif cont < 300000000 * 3/16
+			then if cont2 < 2000000 * 1/2
+				then sel <= "0111";
+				     d   <= "0111001";
+		cont2 := cont2 + 1;
+		
+		elsif cont2 < 2_000_000 * 2/2
+			then sel <= "1011";
+			     d   <= "0111111";
+
+		cont2 := cont2 + 1;
+			else cont2 := 0;
+
+		end if;
+		
+		elsif cont < 300000000 * 4/16
+			then sel <= "0111";
+			     d   <= "0110001";
+
+		elsif cont < 300000000 * 5/16
+			then if cont2 < 2000000 * 1/2
+				then sel <= "0111";
+				     d   <= "1110001";
+		
+		cont2 := cont2 + 1;
+
+		elsif cont2 < 2000000 * 2/2
+			then sel <= "1011";
+			     d   <= "1111011";
+
+		cont2 := cont2 + 1;
+
+		else cont2 := 0;
+		end if;
+		
+		elsif cont < 300000000 * 6/16
+			then if cont2 < 2000000 * 1/2
+				then sel <= "0111";
+				     d   <= "1110011";
+		cont2 := cont2 + 1;
+		
+		elsif cont2 < 2000000 * 2/2
+			then sel <= "1011";
+			     d   <= "1111001";
+		
+		cont2 := cont2 + 1;
+		else cont2 := 0;
+		
+		end if;
+		elsif cont < 300000000 * 7/16
+			then if cont2 < 2000000 * 1/2
+				then sel <= "0111";
+				     d   <= "1110111";
+		cont2 := cont2 + 1;
+		elsif cont2 < 2000000 * 2/2
+			then sel <= "1011";
+			     d   <= "0111001";
+		cont2 := cont2 + 1;
+		else cont2 := 0;
+		end if;
+		
+		elsif cont < 300000000 * 8/16
+			then if cont2 < 2000000 * 1/2
+				then sel <= "1011";
+				     d   <= "0111001";
+		cont2 := cont2 + 1;
+		elsif cont2 < 2000000 * 2/2
+			then sel <= "1101";
+			     d   <= "0111111";
+		cont2 := cont2 + 1;else cont2 := 0;
+		end if;
+
+		elsif cont < 300000000 * 9/16
+			then if cont2 < 2000000 * 1/2
+				then sel <= "1011";
+				     d   <= "0111101";
+		cont2 := cont2 + 1;
+		elsif cont2 < 2000000 * 2/2
+			then sel <= "1101";
+			     d   <= "0011111";
+		cont2 := cont2 + 1;
+		else cont2 := 0;
+		end if;
+
+		elsif cont < 300000000 * 10/16
+			then if cont2 < 2000000 * 1/2
+				then sel <= "1011";
+				     d   <= "0111111";
+		cont2 := cont2 + 1;
+		elsif cont2 < 2000000 * 2/2
+			then sel <= "1101";
+			     d   <= "0001111";
+		cont2 := cont2 + 1;
+		else cont2 := 0;
+		end if;
+
+		elsif cont < 300000000 * 11/16
+			then if cont2 < 2000000 * 1/2
+				then sel <= "1101";
+				     d   <= "0001111";
+		cont2 := cont2 + 1;
+		elsif cont2 < 2000000 * 2/2
+			then sel <= "1110";
+			     d   <= "1110111";
+		cont2 := cont2 + 1;
+		else 										  
+        cont2 :=0;
+		end if;
+		elsif cont < 300_000_000 * 12/16 then  
+			if cont2 < 2_000_000 * 1/2 then    sel <= "1101";
+			d   <= "1001111";
+			cont2 := cont2 + 1;
+			elsif cont2 < 2_000_000 * 2/2 then sel <= "1110";
+		    d   <= "1100111";
+            cont2 := cont2 + 1;
+	    else                              
+            cont2 := 0;
+		end if;
+		elsif cont < 300_000_000 * 13/16 then 	
+			if cont2 < 2_000_000 * 1/2 then    sel <= "1101";
+			d   <= "1101111";
+			cont2 := cont2 + 1;
+		elsif cont2 < 2_000_000 * 2/2 then sel <= "1110";
+			d   <= "1000111";
+			cont2 := cont2 + 1;
+		else	
+            cont2 := 0;
+		end if;
+		elsif cont < 300_000_000 * 14/16 then  sel <= "1110";
+			d   <= "0000111";
+		elsif cont < 300_000_000 * 15/16 then  
+		if cont2 < 2_000_000 * 1/2 then    sel <= "1101";
+			d   <= "0111111";
+			cont2 := cont2 + 1;
+		elsif cont2 < 2_000_000 * 2/2 then sel <= "1110";
+			d   <= "0001111";
+			cont2 := cont2 + 1;
+		else 	
+            cont2 := 0;
+		end if;
+		else                                   
+        if cont2 < 2_000_000 * 1/3 then    sel <= "1011";
+			d   <= "0111111";
+			cont2 := cont2 + 1;
+		elsif cont2 < 2_000_000 * 2/3 then sel <= "1101";
+			d   <= "0111111";
+			cont2 := cont2 + 1;
+		elsif cont2 < 2_000_000 * 3/3 then sel <= "1110";
+			d   <= "0011111";
+				cont2 := cont2 + 1;
+		else
+            cont2 := 0;
+    end if;	
+	end if;
+	end if;
+	end process;
+		
 
 end Behavioral;
-
